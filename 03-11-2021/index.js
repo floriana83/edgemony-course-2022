085e7a7d1952e66b0a850ccadebf17e76ca687ac
moebiusmania <moebiusmania@gmail.com>
2021-11-03 14:04:10 +0100

    feat: 3 nov lesson

---
+++ 03-11-2021/index.js
@@ -0,0 +1,27 @@
+/**
+ * Wraps the document.querySelector method
+ */
+const q = (selector) => document.querySelector(selector);
+
+
+const render = (container, items) => {
+  const elements = items.map((element) => 
+    `<li>
+      <h3>${element.name}</h3>
+      <p><strong>Phone:</strong> <a href="tel:${element.phone}">${element.phone}</a></p>
+      <p><strong>Email:</strong> <a href="mailto:${element.email}">${element.email}</a></p>
+    </li>`
+  );
+  
+  const content = elements.join('');
+
+  container.innerHTML = content;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = q('form');
+  const input = q('form input');
+  const list = q('ul');
+  
+  render(list, data);
+});
