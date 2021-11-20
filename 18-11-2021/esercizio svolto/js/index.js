b0efcfbdc7f4616f58e9c30170591b6efa880453
Floriana Domianello <floriana.domianello@gmail.com>
2021-11-20 17:30:29 +0100

    esercizio migliorato

--- 18-11-2021/esercizio svolto/js/index.js
+++ 18-11-2021/esercizio svolto/js/index.js
@@ -1,32 +1,58 @@
-const btnUp = document.querySelector(".w");
-const btnDw = document.querySelector(".s");
-const btnSx = document.querySelector(".a");
-const btnDx = document.querySelector(".d");
-const dora = document.querySelector(".girl");
+document.addEventListener("DOMContentLoaded", () => {
+    const btnJump = document.querySelector(".w");
+    const btnDw = document.querySelector(".s");
+    const btnSx = document.querySelector(".a");
+    const btnDx = document.querySelector(".d");
 
 
-btnUp.addEventListener("click", () => {
-    dora.classList.toggle("girl1");
-    setTimeout(() => {
-        btnUp.classList.remove("girl1");
-    }, 1000);
-})
-btnDw.addEventListener("click", () => {
-    dora.classList.toggle("girl4");
-    setTimeout(() => {
-        btnDw.classList.remove("girl4");
-    }, 1000);
-})
-btnSx.addEventListener("click", () => {
-    dora.classList.toggle("girl2");
-    setTimeout(() => {
-        btnUp.classList.remove("girl2");
-    }, 1000);
-})
+    const dora = document.querySelector(".girl");
 
-btnDx.addEventListener("click", () => {
-    dora.classList.toggle("girl3");
-    setTimeout(() => {
-        btnUp.classList.remove("girl3");
-    }, 1000);
+
+    let moveleft = 350;
+    let movetop = 180;
+
+    dora.style.left = `${moveleft}px`;
+    dora.style.top = `${movetop}px`;
+
+    btnJump.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        if (movetop > 0) {
+            dora.classList.toggle('animate__bounce');
+            setTimeout(() => dora.classList.toggle('animate__bounce'), 1200);
+        }
+
+    });
+
+    btnDw.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (movetop < 270) {
+            dora.classList.toggle('down');
+            movetop = movetop + 100;
+            dora.style.top = `${movetop}px`;
+            setTimeout(() => dora.classList.toggle('down'), 400);
+        }
+    });
+
+
+    btnSx.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (moveleft > 86) {
+            dora.classList.toggle('left');
+            moveleft = moveleft - 100;
+            dora.style.left = `${moveleft}px`;
+            setTimeout(() => dora.classList.toggle('left'), 400);
+        }
+    });
+
+    btnDx.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (moveleft < 586) {
+            dora.classList.toggle('right');
+            moveleft = moveleft + 100;
+            dora.style.left = `${moveleft}px`;
+            setTimeout(() => dora.classList.toggle('right'), 400);
+        }
+    });
+
 })
